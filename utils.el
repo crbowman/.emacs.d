bb7cff838d2a307ce96e82152446f8ee8caf28db
Curtis Bowman <c.bowman1711@gmail.com>
2014-06-06 15:40:34 -0400

    more sudo edit goodies

--- utils.el
+++ utils.el
@@ -58,4 +58,10 @@ Repeated invocations toggle between the two most recently open buffers."
                          (ido-read-file-name "Find file(as root): ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'utils)
